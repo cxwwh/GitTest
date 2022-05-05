0478da580a7558d281d4afdc94f004f4a87c79b4
cxwwh <1742120929@qq.com>
2022-05-05 17:20:48 +0800

    first_commit

---
+++ src/main/java/wh/git/hello/Hello.java
@@ -0,0 +1,16 @@
+package wh.git.hello;
+
+import com.sun.scenario.effect.impl.prism.ps.PPStoPSWDisplacementMapPeer;
+
+/**
+ * @author CXWWH
+ * @create 2022-05-05-17:15
+ * @
+ **/
+public class Hello {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello Git");
+    }
+}
---
+++ src/main/java/wh/git/hello/Test.java
@@ -0,0 +1,9 @@
+package wh.git.hello;
+
+/**
+ * @author CXWWH
+ * @create 2022-05-05-17:17
+ * @
+ **/
+public class Test {
+}
